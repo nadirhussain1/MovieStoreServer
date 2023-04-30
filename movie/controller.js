648630bb6b6d1a8b3d832692256924c5d7194510
Nadir <nadirhussain1@gmail.com>
2023-04-30 10:49:27 +0500

    sending error and status
    handling errors via status codes and error messages

--- movie/controller.js
+++ movie/controller.js
@@ -1,23 +1,37 @@
 import { getAll, remove, get, save, rate } from './model.js';
 
 export async function listAction(request, response) {
-  const movies = await getAll(1);
-  const moviesResponse = {
-    movies,
-    links: [{ rel: 'self', href: request.baseUrl + '/' }],
-  };
+  try {
+    const movies = await getAll(1);
+    const moviesResponse = {
+      movies,
+      links: [{ rel: 'self', href: request.baseUrl + '/' }],
+    };
 
-  response.json(moviesResponse);
+    response.json(moviesResponse);
+  } catch (e) {
+    console.error(e);
+    response.status(500).send('Server side error');
+  }
 }
 
 
 export async function detailAction(request, response) {
-  const movie = await get(request.params.id,1);
-  const moviesResponse = {
-    movie,
-    links: [{ rel: 'self', href: `${request.baseUrl}/${movie.id}` }], };
+  try {
+    const movie = await get(request.params.id, 1);
+    if(!movie){
+      response.status(404).send("Not Found");
+      return;
+    }
+    const moviesResponse = {
+      movie,
+      links: [{ rel: 'self', href: `${request.baseUrl}/${movie.id}` }],
+    };
 
-  response.json(moviesResponse);
+    response.json(moviesResponse);
+  } catch (e) {
+    response.status(500).send("Server side error");
+  }
 }
 
 export async function removeAction(request, response) {
