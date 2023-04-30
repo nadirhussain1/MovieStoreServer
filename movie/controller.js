b849e300c578c689b3a2c1bbe9291b09b4af9855
Nadir <nadirhussain1@gmail.com>
2023-04-30 12:41:42 +0500

    sort list and xml format
    added support for sorting the list and xml output format

--- movie/controller.js
+++ movie/controller.js
@@ -1,14 +1,50 @@
 import { getAll, remove, get, save, rate } from './model.js';
+import jsonXml from 'jsontoxml';
+
+
+function getLinks(current, base) {
+
+  const links = [
+    { rel: 'base', href: base + '/' },
+    { rel: 'sort-ascending', href: base + '/?sort=asc' },
+    { rel: 'sort-descending', href: base + '/?sort=desc' },
+  ];
+
+  return links.map((link) => {
+    if (current.length > 0 && link.rel.includes(current)) {
+      link.rel = 'self';
+    } else if (current.length === 0 && link.rel === 'base') {
+      link.rel = 'self';
+    }
+    return link;
+  });
+}
+
 
 export async function listAction(request, response) {
   try {
-    const movies = await getAll(1);
-    const moviesResponse = {
-      movies,
-      links: [{ rel: 'self', href: request.baseUrl + '/' }],
+    const options = {
+      userId: 1,
+      sort: request.query.sort ? request.query.sort : '',
     };
 
-    response.json(moviesResponse);
+    const movies = await getAll(options);
+    const moviesResponse = {
+      movies,
+      links: getLinks(options.sort, request.baseUrl),
+    };
+
+    response.format({
+      xml() {
+        moviesResponse.movies = moviesResponse.movies.map((movie) => ({
+          movie,
+        }));
+        response.send(jsonXml(moviesResponse));
+      },
+      json() { response.json(moviesResponse); },
+      default() { response.json(moviesResponse); },
+    });
+
   } catch (e) {
     console.error(e);
     response.status(500).send('Server side error');
@@ -19,7 +55,7 @@ export async function listAction(request, response) {
 export async function detailAction(request, response) {
   try {
     const movie = await get(request.params.id, 1);
-    if(!movie){
+    if (!movie) {
       response.status(404).send("Not Found");
       return;
     }
--- movie/model.js
+++ movie/model.js
@@ -1,12 +1,17 @@
 import sqlite from 'sqlite3';
 const db = new sqlite.Database('./movie.db');
 
-export async function getAll(userId) {
+export async function getAll(options) {
     return new Promise((resolve, reject) => {
 
-        const query = `SELECT * FROM Movies WHERE user = ? OR public = 1`;
+        let query = `SELECT * FROM Movies WHERE user = ? OR public = 1`;
 
-        db.all(query,[userId], (error, results) => {
+        if (options.sort &&
+            ['asc', 'desc'].includes(options.sort.toLowerCase())) {
+                query += ' ORDER BY title ' + options.sort;
+        }
+
+        db.all(query, [options.userId], (error, results) => {
             if (error) {
                 reject(error);
             } else {
@@ -16,10 +21,10 @@ export async function getAll(userId) {
     });
 }
 
-export  async function get(id,userId) {
+export async function get(id, userId) {
     return new Promise((resolve, reject) => {
         const query = 'SELECT * FROM Movies WHERE id = ? AND (user = ? OR public = 1)';
-        db.get(query, [id,userId], (error, results) => {
+        db.get(query, [id, userId], (error, results) => {
             if (error) {
                 reject(error);
             } else {
@@ -29,18 +34,18 @@ export  async function get(id,userId) {
     });
 }
 
-export async function save(movie,userId) {
+export async function save(movie, userId) {
     if (!movie.id) {
-        insert(movie,userId);
+        insert(movie, userId);
     } else {
-        update(movie,userId);
+        update(movie, userId);
     }
 }
 
-export async function remove(id,userId) {
+export async function remove(id, userId) {
     return new Promise((resolve, reject) => {
-        const query = 'DELETE FROM Movies WHERE id = ? AND (user = ? OR public = 1)'; 
-        db.run(query, [id,userId], (error, results) => {
+        const query = 'DELETE FROM Movies WHERE id = ? AND (user = ? OR public = 1)';
+        db.run(query, [id, userId], (error, results) => {
             if (error) {
                 reject(error);
             } else {
@@ -55,15 +60,15 @@ export async function rate(rating) {
     const deleteQuery = 'DELETE FROM Ratings WHERE movie = ? AND user = ?';
     await db.run(deleteQuery, [rating.movie, rating.user]);
     const insertQuery =
-    'INSERT INTO Ratings (movie, user, rating) VALUES (?, ?, ?)';
+        'INSERT INTO Ratings (movie, user, rating) VALUES (?, ?, ?)';
     return db.run(insertQuery, [rating.movie, rating.user, rating.rating]);
 }
-    
 
-function insert(movie,userId) {
+
+function insert(movie, userId) {
     return new Promise((resolve, reject) => {
         const query = 'INSERT INTO Movies (title, year,public, user) VALUES (?, ?,?,?)';
-        db.run(query, [movie.title, movie.year,movie.public, userId], (error, results) => {
+        db.run(query, [movie.title, movie.year, movie.public, userId], (error, results) => {
             if (error) {
                 reject(error);
             } else {
@@ -73,10 +78,10 @@ function insert(movie,userId) {
     });
 }
 
-function update(movie,userId) {
+function update(movie, userId) {
     return new Promise((resolve, reject) => {
         const query = 'UPDATE Movies SET title = ?, year = ?, public = ? , user=? WHERE id = ?';
-        db.run(query, [movie.title, movie.year,movie.public, userId, movie.id], (error, results) => {
+        db.run(query, [movie.title, movie.year, movie.public, userId, movie.id], (error, results) => {
             if (error) {
                 reject(error);
             } else {
