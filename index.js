8810f99f377b46133792f9003209458127afce56
Nadir <nadirhussain1@gmail.com>
2023-04-30 14:47:09 +0500

    added express-jwt
    express-jwt added to validate token before access to movies data

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import express from "express";
 import morgan from 'morgan';
-import {router as movieRouter} from './movie/index.js';
+import { router as movieRouter } from './movie/index.js';
 import { router as loginRouter } from './auth.js';
-
+import { expressjwt as expressJwt } from 'express-jwt';
 
 const app = express();
 
-app.use(morgan('common', { immediate: true}));
+app.use(morgan('common', { immediate: true }));
 
 app.use(express.json());
 
 app.use('/login', loginRouter);
-app.use('/movie', movieRouter);
 
-app.get('/',(request, response) => response.redirect('/movie'));
+app.use( '/movie', expressJwt({ secret: 'secret', algorithms: ['HS256'] }), movieRouter,);
 
-app.listen(8080,() => {
+app.use((err, request, response, next) => {
+
+    if (err.name === 'UnauthorizedError') {
+        response.status(401).json('unauthorized');
+    } else {
+        next();
+    }
+
+});
+
+app.get('/', (request, response) => response.redirect('/movie'));
+
+app.listen(8080, () => {
     console.log('Server is listening at http://localhost:8080');
 })
 
