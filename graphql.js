857b88fcad6a2a2124db29f613007dd0feca5fb1
Nadir <nadirhussain1@gmail.com>
2023-05-06 10:14:58 +0500

    added write operations in graphql
    create, update, delete interfaces added in graphql

--- graphql.js
+++ graphql.js
@@ -1,6 +1,6 @@
 import { buildSchema } from 'graphql';
 import expressGraphql from 'express-graphql';
-import { getAll } from './movie/model.js';
+import { getAll, save ,remove} from './movie/model.js';
 
 const schema = buildSchema(`
   type Movie {
@@ -11,22 +11,55 @@ const schema = buildSchema(`
     user: Int
   }
 
+  input MovieInput { 
+    id: Int
+    title: String! 
+    year: Int 
+    public: Int 
+    user: Int
+  }
+
   type Query {
     greet: String
     movie(id:Int): [Movie]
   }
+
+  type Mutation {
+    createMovie(movie: MovieInput): Movie
+    updateMovie(movie: MovieInput): Movie
+    deleteMovie(id: Int!): Boolean
+  }
+
+
 `);
 
 const rootValue = {
+
   greet() {
     return 'Hello GraphQL';
   },
-  async movie({id}) {
+
+  async movie({ id }) {
     const movies = await getAll({ userId: 1 });
     if (id) {
-    return movies.filter((movie) => movie.id === id); }
+      return movies.filter((movie) => movie.id === id);
+    }
     return movies;
-  }
+  },
+
+  createMovie({ movie }) { 
+    return save(movie, 1); 
+  },
+
+  async updateMovie({ movie }) {
+    await save(movie, 1);
+    return movie;
+  },
+
+  async deleteMovie({ id }) {
+    await remove(id, 1);
+    return true;
+  },
 };
 
 export default expressGraphql.graphqlHTTP({
